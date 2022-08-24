23566ed15f0122dc9ad30906b7b007a8d6a8a0e7
Your Name <deva162501@gmail.com>
2022-08-25 01:22:12 +0530

    script

---
+++ script.js
@@ -0,0 +1,38 @@
+
+function signup() {
+let uname = document.getElementById("uname").value;
+let upass = document.getElementById("upass").value;
+let jsObject = {
+    'name': uname,
+    'pass': upass
+}
+    fetch('http://localhost:3001/signUpData', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(jsObject)
+    }).then(res => {
+        res.text().then(data => {
+            //console.log(data);
+            document.getElementById("demo").innerText = data;
+        })
+    });
+
+}
+
+function login() {
+    let uname = document.getElementById("uname").value;
+    let upass = document.getElementById("upass").value;
+    let jsObject = {
+        'name': uname,
+        'pass': upass
+    }
+    fetch("http://localhost:3001/loginData",{
+        method : 'POST',
+        headers : {'Content-Type' : 'application/json'},
+        body : JSON.stringify(jsObject)
+    })
+    .then(res => {
+        res.text().then(data => console.log(data))
+     }) ;   
+    
+}
